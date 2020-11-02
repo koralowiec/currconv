7fc95faad20b503b0949dfae8f4037f0c45aa80c
koralowiec <arek0906@protonmail.com>
2020-11-02 17:15:49 +0100

    Help flag with examples of usage, printing information about probably misspelled currencies if API returns 400

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -15,7 +16,7 @@ type ApiResponse struct {
 	Rates map[string]float64 `json:"rates"`
 }
 
-func fetchApi(base, target string) float64 {
+func fetchApi(base, target string) (float64, error) {
 	api := "https://api.exchangeratesapi.io/latest"
 	url := api + "?symbols=" + target + "&base=" + base
 	res, err := http.Get(url)
@@ -23,6 +24,10 @@ func fetchApi(base, target string) float64 {
 		fmt.Println(err)
 	}
 	defer res.Body.Close()
+
+	if res.StatusCode == 400 {
+		return 0, fmt.Errorf("API error: Probably misspelled one of currencies: %s, %s", base, target)
+	}
 
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
@@ -35,16 +40,31 @@ func fetchApi(base, target string) float64 {
 		panic(err)
 	}
 
-	return result.Rates[target]
+	return result.Rates[target], nil
 }
 
-func calculateEquivalent(base, target string, amount float64) (float64, float64) {
-	rate := fetchApi(base, target)
-	return amount * rate, rate
+func calculateEquivalent(base, target string, amount float64) (float64, float64, error) {
+	rate, err := fetchApi(base, target)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	return amount * rate, rate, nil
 }
 
 func main() {
 	args := os.Args[1:]
+
+	helpFlag := flag.Bool("h", false, "Show help")
+	flag.Parse()
+
+	if *helpFlag || len(args) < 3 {
+		fmt.Println("Usage:")
+		fmt.Println("currconv 2.5 eur usd")
+		fmt.Println("currconv 100 USD to pln")
+		fmt.Println("currconv 25 GBP USD")
+		return
+	}
 
 	amountOfBaseCurr, err := strconv.ParseFloat(args[0], 32)
 	if err != nil {
@@ -54,7 +74,12 @@ func main() {
 	baseCurrency := strings.ToUpper(args[1])
 	targetCurrency := strings.ToUpper(args[len(args)-1])
 
-	amountOfTargetCurr, rate := calculateEquivalent(baseCurrency, targetCurrency, amountOfBaseCurr)
+	amountOfTargetCurr, rate, err := calculateEquivalent(baseCurrency, targetCurrency, amountOfBaseCurr)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
 	fmt.Printf("%f %s = %f %s\n", amountOfBaseCurr, baseCurrency, amountOfTargetCurr, targetCurrency)
 	fmt.Printf("1 %s = %f %s\n", baseCurrency, rate, targetCurrency)
